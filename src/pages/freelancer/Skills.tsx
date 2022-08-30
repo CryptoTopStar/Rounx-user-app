e0d008c6bf69cc14b04e09e8d7cab00e3220098f
SusanoGit <108870471+CryptoTopStar@users.noreply.github.com>
2022-08-30 09:04:05 +0900

    202208300904

--- src/pages/freelancer/Skills.tsx
+++ src/pages/freelancer/Skills.tsx
@@ -1,25 +1,21 @@
 import { Typography, Box, Paper, Divider, Select, Chip } from "@mui/material";
 import React from "react";
+import { BlueButton } from "../../commonStyle/CommonStyle";
 
 export default function Skills() {
   return (
     <Box style={{ margin: '5% 30%' }}>
-      <Typography marginBottom='20px'>
+      <Typography marginBottom='20px' fontSize='24px'>
         Submit freelancer application
       </Typography>
-      <Paper>
-        <Typography >Skills</Typography>
-        <Typography>Step 1 of 6</Typography>
+      <Paper style={{ padding: '20px' }}>
+        <Typography marginBottom='10px' fontSize='20px' >Skills</Typography>
+        <Typography fontSize='16px' color='#757575'> Step 1 of 6</Typography>
         <Divider />
-        <Select>
-
-        </Select>
-        <Select>
-
-        </Select>
         <Divider />
-        <Typography>Selected skills</Typography>
+        <Typography marginBottom='20px'>Selected skills</Typography>
         <Chip label="Android" />
+        <BlueButton>Next</BlueButton>
       </Paper>
     </Box>
 
