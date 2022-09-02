9ace50bdc1e0c0c7724cfb84d1b346935b714a00
SusanoGit <108870471+CryptoTopStar@users.noreply.github.com>
2022-09-03 00:15:55 +0900

    202209020015

---
+++ src/pages/homepage/PrivacyPolicy.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <Box margin='5% 15%'>
+      <Typography>Privacy Policy</Typography>
+    </Box>
+  )
+}
